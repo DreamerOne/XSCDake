c21756e554bed540e9797fe2e7d6a8416dc266ca
YLTAPPleDeveloper <1958070022@qq.com>
2019-03-02 12:19:44 +0800

    Add A Object

---
+++ XSCDake/XSCDake/Model/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  XSCDake
+//
+//  Created by doctor on 2019/3/2.
+//  Copyright © 2019 doctor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+@property (nonatomic, copy) NSString *name;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XSCDake/XSCDake/Model/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  XSCDake
+//
+//  Created by doctor on 2019/3/2.
+//  Copyright © 2019 doctor. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
